55bd12c7175c7e8b7bdade20f650436da84137cc
MaksKrasn <uw5ekr@gmail.com>
2020-10-01 23:01:55 +0300

    delete old folder

--- ToDoList_DB_FreeMarker/src/main/java/org/home/repositories/TasksRepository.java
+++
@@ -1,8 +0,0 @@
-package org.home.repositories;
-
-import org.home.entities.Task;
-import org.springframework.data.repository.CrudRepository;
-
-public interface TasksRepository extends CrudRepository<Task, Long> {
-    Task findByTaskName(String taskName);
-}
